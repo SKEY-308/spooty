8d78345ee856475e16bd68ad890037d782c61d37
Your Name <severus.key308@gmail.com>
2022-11-20 18:46:52 +0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'spooty',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
